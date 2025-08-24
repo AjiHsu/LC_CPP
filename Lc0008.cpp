f1e69e059e9e614808bdc9d1ab449e7c7a251267
AJI_HSU <122804833+AjiHsu@users.noreply.github.com>
2025-08-24 16:59:43 +0800

    0008

---
+++ Lc0008.cpp
@@ -0,0 +1,27 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int myAtoi(string s) {
+        int index = 0;
+        bool isNeg = false;
+        while (index < s.size() && s[index] == ' ') index++;
+        if (index < s.size() && s[index] == '-') {
+            isNeg = true;
+            index++;
+        }
+        else if (index < s.size() && s[index] == '+') {
+            index++;
+        }
+
+        long long res = 0;
+        while (index < s.size() && '0' <= s[index] && s[index] <= '9') {
+            res *= 10;
+            res += s[index++] - '0';
+            if (isNeg && res >= -((long long)INT_MIN)) return INT_MIN;
+            if (!isNeg && res >= INT_MAX) return INT_MAX;
+        }
+
+        return isNeg ? -res : res;
+    }
+};
